cef9586a521fc853cc8f6f43fbc361ab22cb442a
Arindam Nath <strider2023@gmail.com>
2018-09-14 12:44:09 +0530

    Removed static references

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ var router = require('./src/scripts/router/router');
 var sqlite = require('./src/scripts/db/sqlite/sqlite');
 
 var app = express();
-app.use('/public', express.static('public')); // remote sever public access location
-app.use(multer({ dest: '/uploads/' }).any()); // remote sever upload location
+//app.use('/public', express.static('public')); // remote sever public access location
+//app.use(multer({ dest: '/uploads/' }).any()); // remote sever upload location
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 router.routRequests(app);
